c8dc86a75cf895139df1e925837b91ad0f6d855f
Gordi42 <silvano98@hotmail.de>
2024-03-02 16:38:18 +0100

    created a new list for toml saving and loading

--- src/cluster_state.rs
+++ src/cluster_state.rs
@@ -1,25 +1,59 @@
 use crate::cluster;
-use color_eyre::eyre;
+use crate::toml_list::TomlList;
+use color_eyre::eyre::Result;
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct ClusterState {
     counter: u32,
-    cluster_list: Vec<cluster::Cluster>,
+    cluster_list: TomlList<cluster::Cluster>,
     _new_cluster: cluster::Cluster,
 }
 
 impl ClusterState {
-    pub fn new() -> ClusterState {
-        ClusterState {
+    // =======================================================================
+    //             CONSTRUCTORS
+    // =======================================================================
+    pub fn new_empty() -> Result<ClusterState> {
+        let loaded_list: TomlList<cluster::Cluster> = TomlList::new();
+        Ok(ClusterState {
             counter: 0,
-            cluster_list: Vec::new(),
+            cluster_list: loaded_list,
             _new_cluster: cluster::Cluster::new_empty(),
-        }
+        })
     }
+    pub fn new_load() -> Result<ClusterState> {
+        let loaded_list: TomlList<cluster::Cluster> = TomlList::load("clusters")?;
+        Ok(ClusterState {
+            counter: 0,
+            cluster_list: loaded_list,
+            _new_cluster: cluster::Cluster::new_empty(),
+        })
+    }
+
+    // =======================================================================
+    //  GETTERS AND SETTERS
+    // =======================================================================
 
     pub fn add_cluster(&mut self, cluster: cluster::Cluster) {
         self.cluster_list.push(cluster);
+    }
+
+    pub fn get_cluster(&self) -> Result<&cluster::Cluster> {
+        let index = self.counter as usize;
+        if self.is_new_cluster() {
+            return Ok(&self._new_cluster);
+        }
+        self.cluster_list.get(index)
+    }
+
+    // =======================================================================
+    //  CHECKERS
+    // =======================================================================
+
+    pub fn is_new_cluster(&self) -> bool {
+        // if the counter is at the end of the list, the new cluster is selected
+        self.counter == self.cluster_list.len() as u32
     }
 
     // pub fn cluster_is_valid(self) -> eyre::Result<()> {
@@ -27,53 +61,23 @@ impl ClusterState {
     //     cluster.check_if_cluster_is_valid()
     // }
 
+    // =======================================================================
+    //            FILE OPERATIONS
+    // =======================================================================
 
-    pub fn get_cluster(&self) -> eyre::Result<&cluster::Cluster> {
-        let index = self.counter as usize;
-        if self.is_new_cluster() {
-            return Ok(&self._new_cluster);
-        }
-        let cluster = self.cluster_list.get(index).ok_or_else(|| eyre::eyre!("Cluster index out of bounds."))?;
-        Ok(cluster)
+    pub fn save_cluster_list(&self) -> Result<()> {
+        self.cluster_list.save("clusters")
     }
 
-    pub fn is_new_cluster(&self) -> bool {
-        // if the counter is at the end of the list, the new cluster is selected
-        self.counter == self.cluster_list.len() as u32
-    }
-
-    // pub fn save_cluster_list(&self) -> eyre::Result<()> {
-    //     let home = std::env::var("HOME")?;
-    //     let config_dir = format!("{}/.config/code-remote", home);
-    //     std::fs::create_dir_all(&config_dir)?;
-    //     let file = format!("{}/clusters.json", config_dir);
-    //     let mut clusters = Vec::new();
-    //     for cluster in &self.cluster_list {
-    //         let cluster_json = serde_json::to_string(cluster)?;
-    //         clusters.push(cluster_json);
-    //     }
-    //     let clusters_json = serde_json::to_string(&clusters)?;
-    //     std::fs::write(file, clusters_json)?;
-    //     Ok(())
-    // }
-
-    pub fn save_cluster_list(&self) -> eyre::Result<()> {
-        let home = std::env::var("HOME")?;
-        let config_dir = format!("{}/.config/code-remote", home);
-        std::fs::create_dir_all(&config_dir)?;
-        let file = format!("{}/clusters.toml", config_dir);
-        let toml_str = toml::to_string(&self)?;
-        std::fs::write(file, toml_str)?;
+    pub fn load_cluster_list(&mut self) -> Result<()> {
+        let loaded_list: TomlList<cluster::Cluster> = TomlList::load("clusters")?;
+        self.cluster_list = loaded_list;
         Ok(())
     }
 
-    pub fn load_cluster_list() -> eyre::Result<ClusterState> {
-        let home = std::env::var("HOME")?;
-        let file = format!("{}/.config/code-remote/clusters.toml", home);
-        let toml_str = std::fs::read_to_string(file)?;
-        let cluster_state: ClusterState = toml::from_str(&toml_str)?;
-        Ok(cluster_state)
-    }
+    // =======================================================================
+    //            CONTROLS
+    // =======================================================================
 
     pub fn next(&mut self) {
         let max = self.cluster_list.len() as u32;
@@ -98,18 +102,18 @@ impl ClusterState {
 mod tests {
     use super::*;
 
-    fn create_dummy_cluster_state() -> ClusterState {
-        let mut cluster_state = ClusterState::new();
+    fn create_dummy_cluster_state() -> Result<ClusterState> {
+        let mut cluster_state = ClusterState::new_empty()?;
         let cluster = cluster::Cluster::new("cluster1", "host1", "user1", "identity_file1");
         cluster_state.add_cluster(cluster);
         let cluster = cluster::Cluster::new("cluster2", "host2", "user2", "identity_file2");
         cluster_state.add_cluster(cluster);
-        cluster_state
+        Ok(cluster_state)
     }
 
     #[test]
     fn test_save_cluster_list() {
-        let cluster_state = create_dummy_cluster_state();
+        let cluster_state = create_dummy_cluster_state().unwrap();
         cluster_state.save_cluster_list().unwrap();
         let home = std::env::var("HOME").unwrap();
         let file = format!("{}/.config/code-remote/clusters.toml", home);
@@ -118,10 +122,11 @@ mod tests {
 
     #[test]
     fn test_load_cluster_list() {
-        let dummy_cluster_state = create_dummy_cluster_state();
+        let dummy_cluster_state = create_dummy_cluster_state().unwrap();
         dummy_cluster_state.save_cluster_list().unwrap();
 
-        let mut cluster_state = ClusterState::load_cluster_list().unwrap();
+        let mut cluster_state = ClusterState::new_empty().unwrap();
+        cluster_state.load_cluster_list().unwrap();
         assert_eq!(cluster_state.cluster_list.len(), 2);
         assert_eq!(cluster_state.get_cluster().unwrap().name, "cluster1");
         cluster_state.next();
@@ -130,7 +135,7 @@ mod tests {
 
     #[test]
     fn test_next() {
-        let mut cluster_state = create_dummy_cluster_state();
+        let mut cluster_state = create_dummy_cluster_state().unwrap();
         assert_eq!(cluster_state.counter, 0);
         cluster_state.next();
         assert_eq!(cluster_state.counter, 1);
@@ -142,7 +147,7 @@ mod tests {
 
     #[test]
     fn test_previous() {
-        let mut cluster_state = create_dummy_cluster_state();
+        let mut cluster_state = create_dummy_cluster_state().unwrap();
         cluster_state.previous();
         assert_eq!(cluster_state.counter, 2);
         cluster_state.previous();
@@ -153,7 +158,7 @@ mod tests {
 
     #[test]
     fn test_is_new_cluster() {
-        let mut cluster_state = create_dummy_cluster_state();
+        let mut cluster_state = create_dummy_cluster_state().unwrap();
         assert!(! cluster_state.is_new_cluster());
         cluster_state.next();
         assert!(! cluster_state.is_new_cluster());
---
+++ src/toml_list.rs
@@ -0,0 +1,121 @@
+use crate::cluster;
+use color_eyre::eyre;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct TomlList<T> {
+    entry: Vec<T>,
+}
+
+impl<T: Serialize> TomlList<T> {
+    pub fn new() -> TomlList<T> {
+        TomlList {
+            entry: Vec::new(),
+        }
+    }
+
+    pub fn push(&mut self, item: T) {
+        self.entry.push(item);
+    }
+
+    pub fn get(&self, index: usize) -> eyre::Result<&T> {
+        let entry = self.entry.get(index)
+            .ok_or_else(|| eyre::eyre!("Index out of bounds."))?;
+        Ok(entry)
+    }
+
+    pub fn len(&self) -> usize {
+        self.entry.len()
+    }
+
+    pub fn save(&self, filename: &str) -> eyre::Result<()> {
+        let home = std::env::var("HOME")?;
+        let config_dir = format!("{}/.config/code-remote", home);
+        std::fs::create_dir_all(&config_dir)?;
+        let file = format!("{}/{}.toml", config_dir, filename);
+        let toml_str = toml::to_string(&self)?;
+        // write the toml string to the file
+        // if the file exists, it should be overwritten
+        std::fs::write(file, toml_str)?;
+        Ok(())
+    }
+
+    pub fn load(filename: &str) -> eyre::Result<TomlList<T>> 
+    where for<'de> T: Deserialize<'de> {
+        let home = std::env::var("HOME")?;
+        let file = format!("{}/.config/code-remote/{}.toml", home, filename);
+        // if the file does not exist, return an empty list
+        if !std::path::Path::new(&file).exists() {
+            return Ok(TomlList::new());
+        }
+        // otherwise, load the list
+        let toml_str = std::fs::read_to_string(file)?;
+        let list: TomlList<T> = toml::from_str(&toml_str)?;
+        Ok(list)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let list: TomlList<cluster::Cluster> = TomlList::new();
+        assert_eq!(list.len(), 0);
+    }
+
+    #[test]
+    fn test_push() {
+        let mut list: TomlList<cluster::Cluster> = TomlList::new();
+        let cluster = cluster::Cluster::new("test", "test", "test", "test");
+        list.push(cluster);
+        assert_eq!(list.len(), 1);
+    }
+
+    #[test]
+    fn test_get() {
+        let mut list: TomlList<cluster::Cluster> = TomlList::new();
+        let cluster = cluster::Cluster::new(
+            "mname", "mhost", "muser", "mid");
+        list.push(cluster);
+        let entry = list.get(0).unwrap();
+        assert_eq!(entry.name, "mname");
+        assert_eq!(entry.host, "mhost");
+        assert_eq!(entry.user, "muser");
+        assert_eq!(entry.identity_file, "mid");
+    }
+
+    #[test]
+    fn test_save() {
+        let mut list: TomlList<cluster::Cluster> = TomlList::new();
+        let cluster = cluster::Cluster::new("test", "test", "test", "test");
+        list.push(cluster);
+        list.save("test").unwrap();
+        let home = std::env::var("HOME").unwrap();
+        let file = format!("{}/.config/code-remote/test.toml", home);
+        assert!(std::path::Path::new(&file).exists());
+    }
+
+    #[test]
+    fn test_load() {
+        // Create the list
+        let mut list: TomlList<cluster::Cluster> = TomlList::new();
+        let cluster = cluster::Cluster::new(
+            "mname", "mhost", "muser", "mid");
+        list.push(cluster);
+        // Save the list
+        list.save("test").unwrap();
+
+        // Load the list
+        let loaded_list: TomlList<cluster::Cluster> = TomlList::load("test").unwrap();
+
+        // Test the loaded list
+        assert_eq!(loaded_list.len(), 1);
+        let entry = loaded_list.get(0).unwrap();
+        assert_eq!(entry.name, "mname");
+        assert_eq!(entry.host, "mhost");
+        assert_eq!(entry.user, "muser");
+        assert_eq!(entry.identity_file, "mid");
+    }
+}
